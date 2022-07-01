2725cffbdeaa148e59d49cc7535175ee22f30ec1
kesa <kesa0v0@gmail.com>
2022-07-01 22:28:16 +0900

    적/나 모델 만들기 시작함
    시작만 함 ㄹㅇㅋㅋ

---
+++ Assets/Scripts/Pawn.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pawn : MonoBehaviour
+{
+    // health of the pawn
+    public int health;
+    // sanity of the pawn
+    public int sanity;
+
+
+    
+
+}
